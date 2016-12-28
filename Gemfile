5337f51b2a0f47c0c4e4cf0b6da90268df68b4b2
MpaK <mrak69@gmail.com>
2016-12-28 23:03:53 +0700

    storytime installed

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'devise'
 gem 'storytime'
 
 # gem 'storytime', path: './vendor/storytime'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  storytime_user
 end
